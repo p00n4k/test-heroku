3bbc17969b956c4f1e6b2fe4b95be9e8484e49bf
p00n4k <105700844+p00n4k@users.noreply.github.com>
2024-04-06 00:23:12 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,62 @@
+const express = require('express');
+const mysql = require('mysql');
+const dotenv = require('dotenv');
+const morgan = require('morgan');
+const cors = require('cors'); // Import the cors middleware
+dotenv.config();
+const app = express();
+app.use(express.json());
+app.use(morgan('dev'));
+app.use(cors()); // Use the cors middleware
+
+const connection = mysql.createConnection({
+  host: process.env.host,
+  user: process.env.user,
+  password: process.env.password,
+  database: process.env.database,
+  port: process.env.dbport,
+});
+
+connection.connect((err) => {
+  if (err) {
+    console.log('Error connecting to Db', err);
+    return;
+  }
+  console.log('Mysql Connected...');
+});
+
+//search by id that contains a string
+app.get('/products/search/id/:id', (req, res) => {
+  const id = req.params.id;
+  connection.query(
+    'SELECT * FROM product WHERE product_id LIKE ? LIMIT 50',
+    ['%' + id + '%'],
+    (err, rows) => {
+      if (err) {
+        console.log('Error in query', err);
+        return;
+      }
+      res.send(rows);
+    }
+  );
+});
+
+//search by name that contains a string
+app.get('/products/search/name/:name', (req, res) => {
+  const name = req.params.name;
+  connection.query(
+    'SELECT * FROM product WHERE product_name LIKE ? LIMIT 50',
+    ['%' + name + '%'],
+    (err, rows) => {
+      if (err) {
+        console.log('Error in query', err);
+        return;
+      }
+      res.send(rows);
+    }
+  );
+});
+
+app.listen(process.env.port || 3000, () => {
+  console.log('Server started on port 3000');
+});
